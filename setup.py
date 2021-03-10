5e52f2cc163c70bde2d32602b73fd22ab2c10649
John Stechschulte <john@picknik.ai>
2021-03-10 10:35:18 -0700

    started ROS2 port

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['baldor'],
-    package_dir={'': 'src'}
-)
-
-setup(**d)
+package_name='baldor'
+setup(
+        name=package_name,
+        version='0.1.3',
+        description='The baldor package',
+        author='Francisco Suarez-Ruiz',
+        author_email='fsuarez6@gmail.com',
+        url='http://wiki.ros.org/baldor',
+        packages=['src/' + package_name],
+        data_files=[
+            ('share/' + package_name, ['package.xml']),
+            ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+            ],
+        )
